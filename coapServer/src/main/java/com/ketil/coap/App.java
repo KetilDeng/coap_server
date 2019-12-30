72b1049881f28907ef01eea6824c3a350c58607a
Ketil <ketildeng@gmail.com>
2019-12-30 11:28:41 +0800

    2019/12/30 commit

--- coapServer/src/main/java/com/ketil/coap/App.java
+++ coapServer/src/main/java/com/ketil/coap/App.java
@@ -1,5 +1,4 @@
 package com.ketil.coap;
-
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
@@ -9,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        CoapServer server = new CoapServer(5683);//主机为localhost 端口为默认端口5683
+        CoapServer server = new CoapServer(5684);//主机为localhost 端口为默认端口5683
         server.add(new CoapResource("api") {
             @Override
             public void handleGET(CoapExchange exchange) { //重写处理GET请求的方法
@@ -29,21 +28,6 @@ public class App
             @Override
             public void handleDELETE(CoapExchange exchange) { //重写处理DELETE请求的方法
                exchange.respond(ResponseCode.CONTENT, "DELETE:Hello CoAP!");  
-            }
-
-            @Override
-            public void handleFETCH(CoapExchange exchange) { //重写处理FETCH请求的方法
-               exchange.respond(ResponseCode.CONTENT, "FETCH:Hello CoAP!");  
-            }
-
-            @Override
-            public void handlePATCH(CoapExchange exchange) { //重写处理PATCH请求的方法
-               exchange.respond(ResponseCode.CONTENT, "PATCH:Hello CoAP!");  
-            }
-
-            @Override
-            public void handleIPATCH(CoapExchange exchange) { //重写处理IPATCH请求的方法
-               exchange.respond(ResponseCode.CONTENT, "IPATCH:Hello CoAP!");  
             }
         });
       server.start();
